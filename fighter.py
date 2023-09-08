d7baf2f837873215da0e13128cb0fe39c5623a85
BGC1990 <bencrayston1990@gmail.com>
2023-09-08 16:36:08 +0100

    Add screen dimensions and captions

---
+++ fighter.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+CAPTION = "Brawler"
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption(CAPTION)
