6b7f489f7101b0f19222c3a928290a6aa008c034
BGC1990 <bencrayston1990@gmail.com>
2023-09-08 17:42:21 +0100

    Make main.py functional

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 1600
-SCREEN_HEIGHT = 1200
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
 CAPTION = "Brawler"
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption(CAPTION)
 
+background_image = pygame.image.load("assets/images/background/background.png").convert_alpha()
+def draw_background():
+    screen.blit(background_image, (0, 0))
+
 run = True
 while run:
+    draw_background()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
+
+pygame.quit()
