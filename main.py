a6fabac3190b7ae767a9d66832350ac6876f1c1f
BGC1990 <bencrayston1990@gmail.com>
2023-09-11 14:58:20 +0100

    Add FPS setting

--- main.py
+++ main.py
@@ -10,15 +10,19 @@ CAPTION = "Brawler"
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption(CAPTION)
 
+clock = pygame.time.Clock()
+FPS = 60
+
 background_image = pygame.image.load("assets/images/background/background.png").convert_alpha()
 def draw_background():
     screen.blit(background_image, (0, 0))
 
-fighter_1 = Fighter(200, 310)
-fighter_2 = Fighter(700, 310)
+fighter_1 = Fighter(200, 400)
+fighter_2 = Fighter(700, 400)
 
 run = True
 while run:
+    clock.tick(FPS)
     draw_background()
     fighter_1.move()
     fighter_2.move()
