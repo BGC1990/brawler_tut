9646a6d654e3b5f442cab94b996a06ba0b483afa
BGC1990 <bencrayston1990@gmail.com>
2023-09-15 17:23:55 +0100

    Add attack method, preliminarily draw attack

--- fighter.py
+++ fighter.py
@@ -5,8 +5,9 @@ class Fighter():
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
+        self.attack_type = 0
 
-    def move(self, screen_width, screen_height):
+    def move(self, screen_width, screen_height, surface):
         SPEED = 10
         GRAVITY = 2
         dx = 0
@@ -23,6 +24,14 @@ class Fighter():
         if key[pygame.K_w] and self.jump == False:
             self.vel_y = -30
             self.jump = True
+        #attack
+        if key[pygame.K_r] or key[pygame.K_t]:
+            self.attack(surface)
+            #determine which attack
+            if key[pygame.K_r]:
+                self.attack_type = 1
+            if key[pygame.K_t]:
+                self.attack_type = 2
 
         #apply gravity
         self.vel_y += GRAVITY
@@ -41,6 +50,10 @@ class Fighter():
 
         self.rect.x += dx
         self.rect.y += dy
+
+    def attack(self, surface):
+        attacking_rect = pygame.Rect(self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
+        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
