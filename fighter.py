bdfb3f931857ddfb06612c421e1c7d75234c44c6
BGC1990 <bencrayston1990@gmail.com>
2023-09-20 16:40:24 +0100

    Scale and offset fighter sprites

--- fighter.py
+++ fighter.py
@@ -3,8 +3,13 @@ import pygame
 class Fighter():
     def __init__(self, x, y, data, sprite_sheet, animation_steps):
         self.size = data[0]
+        self.image_scale = data[1]
+        self.offset = data[2]
         self.flip = False
         self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        self.action = 0
+        self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
@@ -19,7 +24,7 @@ class Fighter():
             temp_img_list = []
             for x in range(animation):
                 temp_img = sprite_sheet.subsurface(x * self.size, y * self.size, self.size, self.size)
-                temp_img_list.append(temp_img)
+                temp_img_list.append(pygame.transform.scale(temp_img, (self.size * self.image_scale, self.size * self.image_scale)))
             animation_list.append(temp_img_list)
         return animation_list
 
@@ -86,3 +91,4 @@ class Fighter():
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        surface.blit(self.image, (self.rect.x - (self.offset[0] * self.image_scale),  self.rect.y - (self.offset[1] * self.image_scale)))
